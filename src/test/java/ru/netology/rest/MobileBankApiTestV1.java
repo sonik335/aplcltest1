7a10928ab0f14a504ab2c6a7798080c08173450f
Kirill <sonik3000.83@gmail.com>
2022-10-05 01:21:44 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,36 @@
+package ru.netology.rest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+    }
+
+    @Test
+    void shouldReturnCurrency() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("RUB"))
+                .body("[1].currency", equalTo("USD"));
+    }
+
+
+}
